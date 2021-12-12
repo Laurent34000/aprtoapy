d8787667635946ff9593dd0f20563390bcbe8191
laurent34000 <lafrigole@hotmail.fr>
2021-12-12 21:23:13 +0100

    header update

--- ata/src/App.js
+++ ata/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-          Ton application AtA est en cours de développement :)
+          En cours de développement
 
       </header>
 
